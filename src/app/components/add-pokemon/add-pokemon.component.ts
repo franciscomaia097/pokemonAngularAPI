f1a6ba576d0a547872ad7e473d97a854ba30fee0
franciscomaia097 <105322731+franciscomaia097@users.noreply.github.com>
2024-02-12 14:33:57 +0000

    Add add-pokemon css and back button

--- src/app/components/add-pokemon/add-pokemon.component.ts
+++ src/app/components/add-pokemon/add-pokemon.component.ts
@@ -1,18 +1,31 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-pokemon',
   templateUrl: './add-pokemon.component.html',
-  styleUrls: ['./add-pokemon.component.css']
+  styleUrls: ['./add-pokemon.component.css'],
 })
 export class AddPokemonComponent {
+  constructor(private router: Router) {}
   pokemonName: string = '';
+  pokemonWeight: number = 0;
+  pokemonHeight: number = 0;
+  pokemonType: string = '';
+  pokemonImageUrl: string = '';
 
   onSubmit(): void {
     if (this.pokemonName) {
-      // Add your logic for adding a Pokemon here
       console.log(`Added Pokemon: ${this.pokemonName}`);
       this.pokemonName = '';
+      this.pokemonWeight = 0;
+      this.pokemonHeight = 0;
+      this.pokemonType = '';
+      this.pokemonImageUrl = '';
     }
+  }
+
+  onBackClick(): void {
+    this.router.navigate(['/']);
   }
 }
